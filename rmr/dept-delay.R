defe3d76e90ef70dc3639e62f5939b7b66f68e9f
Jongwook Woo <jwoo5@calstatela.edu>
2013-04-23 10:08:37 -0700

    update code

--- rmr/dept-delay.R
+++ rmr/dept-delay.R
@@ -12,10 +12,10 @@ deptdelay = function (input, output) {
   mapreduce(input = input,
             output = output,
             #textinputformat = csvtextinputformat,
-            #input.format = "csv", # csvtextinputformat,
-            input.format = make.input.format("csv", sep=","),
-            output.format = make.output.format("csv", sep=","),
-            vectorized = TRUE, structured = TRUE,
+            input.format = csvtextinputformat, #"csv", # 
+            #input.format = make.input.format("csv", sep=","),
+            #output.format = make.output.format("csv", sep=","),
+            #vectorized = TRUE, structured = TRUE,
             map = function(k, fields) {
               # Skip header lines and bad records:
               if (!(identical(fields[[1]], "Year")) & length(fields) == 29) {
